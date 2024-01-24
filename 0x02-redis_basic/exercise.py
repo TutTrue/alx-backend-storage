64b0145cd753bb7e3ae1a086311dd925d0e7c886
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-24 14:05:29 +0200

    decorator that counts the number of function calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,20 @@
 """ Cache module """
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
 import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Count calls decorator """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -12,26 +25,24 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
+    @count_calls
     def store(self, data: Union[bytes, str, int, float]) -> str:
         """ Store data in redis """
         r = str(uuid.uuid4())
         self._redis.set(r, data)
         return r
 
-
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[bytes, str, int, float]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[bytes, str, int, float]:
         """ Get data from redis """
         data = self._redis.get(key)
         if fn:
             data = fn(data)
         return data
 
-
     def get_str(self, key: str) -> str:
         """ Get string from redis """
         return self.get(key, fn=lambda d: d.decode("utf-8"))
-
 
     def get_int(self, key: str) -> int:
         """ Get int from redis """
