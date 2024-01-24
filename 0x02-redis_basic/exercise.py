e1aa3deae81d7b07fd9b39e780ac746d44a77305
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-24 14:18:17 +0200

    call_history decorator to store the history of inputs and outputs for a particular function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ Call history decorator """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        key = method.__qualname__
+        input = str(args)
+        self._redis.rpush(f"{key}:inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(f"{key}:outputs", output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     """ Cache class """
     def __init__(self):
@@ -26,6 +41,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[bytes, str, int, float]) -> str:
         """ Store data in redis """
         r = str(uuid.uuid4())
