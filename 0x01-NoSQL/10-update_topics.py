6a523df61ebda90f77a31defd90f3b24d51d433c
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-23 17:56:05 -0800

    Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Change school topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name """
+    mongo_collection.update_one({ "name": name }, { "$set": { "topics": topics } })
